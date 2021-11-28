e939c889c7ba90acf57b1834b6b8d4f8498bdab2
Qi Yang <qiyang@mail.ustc.edu.cn>
2021-11-28 15:36:00 +0800

    add tJ model

--- src/hamiltonian_zoo/SpinfulFermionicHamiltonian.jl
+++ src/hamiltonian_zoo/SpinfulFermionicHamiltonian.jl
@@ -1,5 +1,5 @@
 using ITensors
-struct KitaevFermionsModel
+struct KitaevFermionsModel <: AbstractElectronHamiltonian
     t::Real
 	Δ::Real
 	μ::Real
@@ -22,6 +22,28 @@ function Hamiltonian(model::KitaevFermionsModel)
     ampo += 0.5*model.μ,"Cdagup",2,"Cup",2
     ampo += 0.5*model.μ,"Cdagdn",1,"Cdn",1
     ampo += 0.5*model.μ,"Cdagdn",2,"Cdn",2
+
+    return Hamiltonian(ampo,"Electron",2)
+end
+
+struct tJModel <: AbstractElectronHamiltonian
+    t::Real
+    J::Real
+end
+
+function Hamiltonian(model::tJModel)
+    ampo = AutoMPO()
+    
+    ampo += -model.t ,"Cdagup",1,"Cup",2
+    ampo += -model.t ,"Cdagup",2,"Cup",1
+    ampo += -model.t ,"Cdagdn",1,"Cdn",2
+    ampo += -model.t ,"Cdagdn",2,"Cdn",1
+    
+    ampo += 0.5*model.J ,"Sz",1,"Sz",2
+    ampo += 0.25*model.J ,"S+",1,"S-",2
+    ampo += 0.25*model.J ,"S-",1,"S+",2
+
+    ampo += -0.125*model.J, "Ntot",1,"Ntot",2
 
     return Hamiltonian(ampo,"Electron",2)
 end
--- test/hamiltonian_zoo/SpinfulFermionicHamiltonian.jl
+++ test/hamiltonian_zoo/SpinfulFermionicHamiltonian.jl
@@ -1,8 +1,16 @@
 using Test,EasyHamiltonian,ITensors
-using EasyHamiltonian:KitaevFermionsModel
+using EasyHamiltonian:KitaevFermionsModel,tJModel
 
 @testset "KitaevFermionsModel" begin
     model = KitaevFermionsModel(1.0,1.0,1.0)
+    hamiltonian = Hamiltonian(model)
+    @test isa(hamiltonian,Hamiltonian)
+    @test isa(Matrix(hamiltonian),Matrix)
+    @test isa(MPO(hamiltonian),MPO)
+end
+
+@testset "tJModel" begin
+    model = tJModel(1.0,1.0)
     hamiltonian = Hamiltonian(model)
     @test isa(hamiltonian,Hamiltonian)
     @test isa(Matrix(hamiltonian),Matrix)
